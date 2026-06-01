18861c61409c1b5b3dbfce9630161e6c29d8f983
Valentin Jimenez <alaraz1921@gmail.com>
2026-06-01 17:51:32 +0200

    Prevent double tap zoom on magic clock

--- assets/js/valentina-magic-clock.js
+++ assets/js/valentina-magic-clock.js
@@ -15,6 +15,15 @@
         let userMinutes = 0;
         let score = 0;
         let confirmCallback = null;
+        let lastTouchEnd = 0;
+
+        document.addEventListener('touchend', (event) => {
+            const now = Date.now();
+            if (now - lastTouchEnd <= 300) {
+                event.preventDefault();
+            }
+            lastTouchEnd = now;
+        }, { passive: false });
 
         function updateClock() {
             // Calcular ángulos
